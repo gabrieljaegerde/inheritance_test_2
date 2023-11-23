1ad25ac9c03162a1413a4c93f715614fa3dd1ed9
Gabriel <gabrieljaeger1@gmail.com>
2023-11-23 10:54:13 +0800

    updated

--- src/main/java/com/example/Appliance.java
+++ src/main/java/com/example/Appliance.java
@@ -1,22 +1,6 @@
 package com.example;
 
 public class Appliance {
-    
-
-    // Constructor to initialize an ElectronicDevice object
-    public Appliance(String brand, String model) {
-        // TODO: Initialize the common attributes (brand, model) with the provided parameters
-    }
-
-    // TODO: Implement getters (and setters if necessary) for the brand and model.
-
-    // OPTIONALLY implement the required methods to pass the BonusTest.
-
-}
-
-package com.example;
-
-public class Appliance {
     // Attributes
     // TODO: Declare common attributes for electronic devices, such as brand and model and powerUsage.
 
